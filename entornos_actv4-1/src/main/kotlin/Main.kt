b683dea57d9220681ccea0651cb56e720352b4f7
MaykaGR <mcgonzalez@iesrafaelalberti.es>
2022-01-26 18:29:42 +0100

    update

--- entornos_actv4-1/src/main/kotlin/Main.kt
+++ entornos_actv4-1/src/main/kotlin/Main.kt
@@ -8,6 +8,22 @@ menos que 7 aprobado, entre 7 y 8 notable y entre 9 y 10 incluido sobresaliente.
 diferente a este rango, que diga “el valor introducido no es correcto”.
  */
 
-fun main() {
+fun notaAlfab (nota: Int): String{
+    val mensaje = when{
+        nota < 5 ->  "Suspenso"
+        nota >= 5 ->  "Aprobado"
+        nota < 7 -> "Aprobado"
+        nota >=7 -> "Notable"
+        nota <= 8 -> "Notable"
+        nota >= 9 -> "Sobresaliente"
+        nota == 10 -> "Sobresaliente"
+        else -> "Error"
+    }
+    return mensaje
+}
 
+fun main() {
+    println("Introduce la nota: ")
+    val nota = readLine()?.toInt()?: 0
+    println(notaAlfab(nota))
 }
